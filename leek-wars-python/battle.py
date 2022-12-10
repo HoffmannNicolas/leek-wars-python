78dbd3ff450d5632bb7015e81bb85924aa37158f
Nicolas Hoffmann <hoffmann1nicolas@gmail.com>
2022-12-10 01:54:33 +0100

    Add sampler

--- leek-wars-python/battle.py
+++ leek-wars-python/battle.py
@@ -1,9 +1,19 @@
 
+from .terrain import Terrain
+from .entity.leek import Leek
+from .entity.turret import Turret
+from .entity.chest import Chest
+import random
 
 
 class Battle:
 
     """ Battle environment that contains everything about a fight """
+
+    GAMEMODE_SOLO = 0
+    GAMEMODE_BATTLE_ROYALE = 1
+    GAMEMODE_FARMER = 2
+    GAMEMODE_TEAM = 3
 
     def __init__(self, terrain, entities):
         self.terrain = terrain
@@ -52,3 +62,74 @@ class Battle:
         while not(self.fightIsOver()):
             player = self.getPlayer()
             self.runPlayerTurn(player)
+
+
+    def __str__(self):
+        toPrint = ""
+        toPrint += f"[Battle]\n"
+        toPrint += str(self.terrain) + '\n'
+        for entity in self.entities:
+            toPrint += str(entity)
+        return toPrint
+
+
+    @classmethod
+    def sample(cls, map_width=17, map_height=17, obstacle_proportion=0.15, gamemode=None, chest_probability=0.01):
+        terrain = Terrain.sample(width=map_width, height=map_height, obstacle_proportion=obstacle_proportion)
+        if (gamemode is None):
+            gamemode = random.choice([Battle.GAMEMODE_SOLO, Battle.GAMEMODE_TEAM, Battle.GAMEMODE_FARMER, Battle.GAMEMODE_BATTLE_ROYALE])
+
+        def _placeEntityRandomly(entity, terrain, otherEntities):
+            while True: # TODO : Fix this possibly infinite loop (if map is not big enough)
+                x = random.randint(0, terrain.width)
+                y = random.randint(0, terrain.height)
+                if (terrain.cell_is_not_free(x, y)): # Sampled cell contains an obstacle
+                    continue
+                for otherEntity in otherEntities: # Sample cell contains another entity already
+                    if (x == otherEntity.current_x_coordinate and y == otherEntity.current_y_coordinate):
+                        pass
+                entity.current_x_coordinate = x
+                entity.current_y_coordinate = y
+                return
+
+        def _addSampledEntity(team, cls):
+            entity = cls.sample()
+            entity.current_team = team
+            _placeEntityRandomly(entity, terrain, entities)
+            entities.append(entity)
+
+        entities = []
+        if (gamemode == Battle.GAMEMODE_SOLO):
+            _addSampledEntity(0, Leek)
+            _addSampledEntity(1, Leek)
+
+        elif (gamemode == Battle.GAMEMODE_TEAM):
+            for i_leek in range(6):
+                _addSampledEntity(0, Leek)
+                _addSampledEntity(1, Leek)
+            _addSampledEntity(0, Turret)
+            _addSampledEntity(1, Turret)
+
+        elif (gamemode == Battle.GAMEMODE_FARMER):
+            for i_leek in range(4):
+                _addSampledEntity(0, Leek)
+                _addSampledEntity(1, Leek)
+
+        elif (gamemode == Battle.GAMEMODE_BATTLE_ROYALE):
+            for i_leek in range(10):
+                _addSampledEntity(i_leek, Leek)
+
+        else:
+            print("ERROR : Unrecognized gamemode")
+            exit() # TODO : Raise proper error instead
+
+        if (random.random() < chest_probability):
+            _addSampledEntity(-1, Chest)
+
+        return cls(terrain, entities)
+
+
+
+if (__name__ == "__main__"):
+    battle = Battle.sample()
+    print(battle)
